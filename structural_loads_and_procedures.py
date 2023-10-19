47e7bbb1bce3b5ae4f2576a911670e3070cf3a00
GabPoulin <poulin33@me.com>
2023-10-19 01:09:35 -0400

    changer en dataclass

--- structural_loads_and_procedures.py
+++ structural_loads_and_procedures.py
@@ -1,110 +1,93 @@
 """
 Script pour effectuer les calculs selon la section
-4.1. Charges et methodes de calculs du CNB-2020.
+4.1. Charges et methodes de calculs du CNB2020.
+
+Auteur: GabPoulin
+email: poulin33@me.com
 """
+from dataclasses import dataclass
 
 
+@dataclass
 class LimitStatesDesign:
-    """Calcul de ELU ou ELTS selon la section 4.1.3."""
+    """Calcul ELU ou ELTS selon CNB-2020: section 4.1.3."""
 
-    def __init__(
-        self,
-        dead=0,
-        live=0,
-        snow=0,
-        wind=0,
-        earthquake=0,
-        storage_area="no",
-        exterior_area="no",
-        car_access="no",
-    ):
-        self.d = dead
-        self.l = live
-        self.s = snow
-        self.w = wind
-        self.e = earthquake
-        self.storage = storage_area
-        self.ext = exterior_area
-        self.car = car_access
+    dead: int | float = 0
+    live: int | float = 0
+    snow: int | float = 0
+    wind: int | float = 0
+    earthquake: int | float = 0
+    storage_area: bool = False
+    exterior_area: bool = False
+    car_access: bool = False
 
-    def elu(self, counter_d="no", liquid_l="no", h_s=0):
-        """Calcul l'ELU selon le Tableau 4.1.3.2.-A du CNB-2020.
+    def elu(self, counter_d=False, liquid_l=False, h_s=0):
+        """Calcul ELU selon CNB-2020: Tableau 4.1.3.2.-A.
 
         Args:
-            counter_d (optional): Charge permanente pondérée contraire. Defaults to "no".
-            liquid_l (optional): Coeff. de surcharge réduit pour liquides. Defaults to "no".
-            h_s (optional): Profondeur du sol, en m, supporté par la structure. Defaults to "no".
+            counter_d (optional): Charge permanente pondérée contraire. Defaults to False.
+            liquid_l (optional): Coeff. de surcharge réduit pour liquides. Defaults to False.
+            h_s (optional): Profondeur du sol, en m, supporté par la structure. Defaults to 0.
 
         Returns:
             float: État Limite Ultime
         """
 
-        # Pondération sur D
-        d1_factor = 1.4
-        d_factor = 1.25
-        if h_s > 0:
-            d1_factor = 1.5  # 4.1.3.2. 9)
-            d_factor = 1.5  # 4.1.3.2. 8)
-            if h_s > 1.2:
-                d_factor = max(1 + 0.6 / h_s, 1.25)  # 4.1.3.2. 8)
-        if counter_d != "no":
-            d_factor = 0.9  # 4.1.3.2. 5)
+        d1 = 1.4
+        d_ = 1.25
 
-        # Pondération sur L
-        pr_l_factor = 1.5
-        if liquid_l != "no":
-            pr_l_factor = 1.25  # 4.1.3.2. 6)
-        co_l_factor = 0
-        if self.storage != "no":
-            co_l_factor = 0.5  # 4.1.3.2. 7)
+        l2 = 1.5
+        l3 = 1
+        l4 = 0.5
+        l5 = 0.5
+
+        s2 = 1
+        s5 = 0.25
+
+        # 4.1.3.2.
+        # 6)
+        if liquid_l:
+            l2 = 1.25
+        # 7)
+        if self.storage_area:
+            l3 += 0.5
+            l4 += 0.5
+        # 8) & 9)
+        if h_s > 0:
+            d_ = 1.5
+            if h_s > 1.2:
+                d_ = max(1 + 0.6 / h_s, 1.25)
+            d1 = 1.5
+        # 5)
+        if counter_d:
+            d_ = 0.9
 
         # 4.1.5.5.
-        co_ls_factor = 1
-        co_l5_factor = 1
-        co_s5_factor = 1
-        co_ls_red_factor = 1
-        co_l5_red_factor = 1
-        co_s5_red_factor = 1
-        if self.ext != "no":
-            if self.car == "no":
-                co_ls_factor = 0
-                if (0.5 + co_l_factor) * self.l < 0.25 * self.s:
-                    co_l5_factor = 0
-                else:
-                    co_s5_factor = 0
-            co_ls_red_factor = 0.2
-            co_l5_red_factor = 0.4
-            co_s5_red_factor = 0.8
+        # co_ls_factor = 1
+        # co_l5_factor = 1
+        # co_s5_factor = 1
+        # co_ls_red_factor = 1
+        # co_l5_red_factor = 1
+        # co_s5_red_factor = 1
+        # if self.exterior_area:
+        #     if not self.car_access:
+        #         co_ls_factor = 0
+        #         if (0.5 + co_l_factor) * self.l < 0.25 * self.snow:
+        #             co_l5_factor = 0
+        #         else:
+        #             co_s5_factor = 0
+        #     co_ls_red_factor = 0.2
+        #     co_l5_red_factor = 0.4
+        #     co_s5_red_factor = 0.8
 
         # Combinaisons de charges
-        cas_1 = d1_factor * self.d
-        cas_2 = (
-            d_factor * self.d
-            + pr_l_factor * self.l
-            + max(self.s * co_ls_factor * co_ls_red_factor, 0.4 * self.w)
-        )
-        cas_3 = (
-            d_factor * self.d
-            + 1.5 * self.s
-            + max(
-                (1 + co_l_factor) * self.l * co_ls_factor * co_ls_red_factor,
-                0.4 * self.w,
-            )
-        )
-        cas_4 = (
-            d_factor * self.d
-            + 1.4 * self.w
-            + max((0.5 + co_l_factor) * self.l, 0.5 * self.s)
-        )
-        cas_5 = (
-            self.d
-            + self.e
-            + (0.5 + co_l_factor) * self.l * co_l5_red_factor * co_l5_factor
-            + 0.25 * self.s * co_s5_red_factor * co_s5_factor
-        )
+        c_1 = d1 * self.dead
+        c_2 = d_ * self.dead + l2 * self.live + max(s2 * self.snow, 0.4 * self.wind)
+        c_3 = d_ * self.dead + 1.5 * self.snow + max(l3 * self.live, 0.4 * self.wind)
+        c_4 = d_ * self.dead + 1.4 * self.wind + max(l4 * self.live, 0.5 * self.snow)
+        c_5 = self.dead + self.earthquake + l5 * self.live + s5 * self.snow
 
-        # ELU max
-        elu = max(cas_1, cas_2, cas_3, cas_4, cas_5)
+        elu = max(c_1, c_2, c_3, c_4, c_5)
 
         return elu
 
@@ -113,16 +96,54 @@ class LimitStatesDesign:
 
 
 # tests
-if __name__ == "__main__":
-    plancher_test = LimitStatesDesign(dead=1, live=1)
-    print(f"---test plancher---\n{plancher_test.elu()}")
+def main():
+    """Série de tests pour les fonctions du fichier."""
 
-    parking_test = LimitStatesDesign(
-        dead=5,
-        live=4.8,
-        snow=3,
-        storage_area="no",
-        exterior_area="yes",
-        car_access="yes",
-    )
-    print(f"---test parking---\n{parking_test.elu(h_s=1)}")
+    dead = LimitStatesDesign(dead=1).elu()
+    res = 1.4
+    if dead != res:
+        print("dead -> FAILED")
+        print(f"result = {dead}")
+        print(f"expected = {res}\n")
+    else:
+        print("dead -> PASSED\n")
+
+    live = LimitStatesDesign(live=1).elu()
+    res = 1.5
+    if live != res:
+        print("live -> FAILED")
+        print(f"result = {live}")
+        print(f"expected = {res}\n")
+    else:
+        print("live -> PASSED\n")
+
+    snow = LimitStatesDesign(snow=1).elu()
+    res = 1.5
+    if snow != res:
+        print("snow -> FAILED")
+        print(f"result = {snow}")
+        print(f"expected = {res}\n")
+    else:
+        print("snow -> PASSED\n")
+
+    wind = LimitStatesDesign(wind=1).elu()
+    res = 1.4
+    if wind != res:
+        print("wind -> FAILED")
+        print(f"result = {wind}")
+        print(f"expected = {res}\n")
+    else:
+        print("wind -> PASSED\n")
+
+    earthquake = LimitStatesDesign(earthquake=1).elu()
+    res = 1
+    if earthquake != res:
+        print("earthquake -> FAILED")
+        print(f"result = {earthquake}")
+        print(f"expected = {res}\n")
+    else:
+        print("earthquake -> PASSED\n")
+
+
+if __name__ == "__main__":
+    main()
